6afbcf74b2c2ecf9d4fbb7abfbd9b7322cc83c50
descmd1 <aondoakaachris@gmail.com>
2022-12-21 08:38:32 +0000

    added files to cart page

--- feature/frontendtask/src/App.js
+++ feature/frontendtask/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import SignUpForm from './components/SignUpForm';
+import Cart from './components/UI/Cart';
 import HomePage from './components/UI/HomePage';
+import MainCart from './components/UI/MainCart';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <MainCart/>
+      {/* <HomePage/> */}
      {/* <SignUpForm/> */}
     </div>
   );
